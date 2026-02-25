0ebba6ad93aba37a01a8809d7c08632a6c3a2731
JustDav3 <elektrobrn22@gmail.com>
2026-02-25 19:16:44 +0000

    beta verze 0.3.007

--- main/views.py
+++ main/views.py
@@ -106,13 +106,13 @@ def home(request):
         # --- 4. SPIRÁLA ---
         elif project == "spirala":
             matrix, dim = matrix_logic.vytvor_matice_sifry(user_input, "1", dynamic_val)
-            user_input = user_input.upper()
+            matrix = matrix.upper()
             context['start_bod'] = dynamic_val # Pro JS vzpomínku
 
         # --- 5. ŠNEK ---
         elif project == "snek":
             matrix, dim = matrix_logic.vytvor_matice_sifry(user_input, "2", dynamic_val)
-            user_input = user_input.upper()
+            matrix = matrix.upper()
             context['start_bod'] = dynamic_val # Pro JS vzpomínku
 
         # --- 6. HAD ---
@@ -121,6 +121,7 @@ def home(request):
             # "shora" -> parametr "2", "zleva" -> parametr "1"
             param_had = "2" if dynamic_val == "shora" else "1"
             matrix, dim = matrix_logic.vytvor_matice_sifry(user_input, "3", param_had)
+            matrix = matrix.upper()
             context['smer_had'] = dynamic_val # Pro JS vzpomínku (shora/zleva)
 
         if project in ["spirala", "snek", "had"]:
