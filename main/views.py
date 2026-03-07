84e6608ec037ff763e4dbbdb9d340db7a8c6537e
JustDav3 <elektrobrn22@gmail.com>
2026-03-07 17:10:17 +0000

    beta verze 0.3.012

--- main/views.py
+++ main/views.py
@@ -108,13 +108,19 @@ def home(request):
         # --- 4. SPIRÁLA ---
         elif project == "spirala":
             matrix, dim = matrix_logic.vytvor_matice_sifry(user_input, "1", dynamic_val)
-            matrix = [prvky.upper() for prvky in matrix]
+            matrix = [
+            [znak.upper() for znak in radek if znak.isalpha()] 
+            for radek in matrix
+            ]
             context['start_bod'] = dynamic_val # Pro JS vzpomínku
 
         # --- 5. ŠNEK ---
         elif project == "snek":
             matrix, dim = matrix_logic.vytvor_matice_sifry(user_input, "2", dynamic_val)
-            matrix = [prvky.upper() for prvky in matrix]
+            matrix = [
+            [znak.upper() for znak in radek if znak.isalpha()] 
+            for radek in matrix
+            ]
             context['start_bod'] = dynamic_val # Pro JS vzpomínku
 
         # --- 6. HAD ---
@@ -123,7 +129,10 @@ def home(request):
             # "shora" -> parametr "2", "zleva" -> parametr "1"
             param_had = "2" if dynamic_val == "shora" else "1"
             matrix, dim = matrix_logic.vytvor_matice_sifry(user_input, "3", param_had)
-            matrix = [prvky.upper() for prvky in matrix]
+            matrix = [
+            [znak.upper() for znak in radek if znak.isalpha()] 
+            for radek in matrix
+            ]
             context['smer_had'] = dynamic_val # Pro JS vzpomínku (shora/zleva)
 
         if project in ["spirala", "snek", "had"]:
