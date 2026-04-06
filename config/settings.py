72ebea6ed2ad530f77d195aaeb66a7d09a848c87
JustDav3 <elektrobrn22@gmail.com>
2026-04-06 14:24:08 +0000

    databaze

--- config/settings.py
+++ config/settings.py
@@ -74,10 +74,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+db_path = os.environ.get('data', os.path.join(BASE_DIR, 'data_np00'))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': db_path,
     }
 }
 
