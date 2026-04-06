9409c7aac7c442354d443ac6ef31c02a220dda47
JustDav3 <elektrobrn22@gmail.com>
2026-04-06 16:17:03 +0000

    databaze - propojení

--- createsuperuser.py
+++ createsuperuser.py
@@ -7,9 +7,9 @@ django.setup()
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-username = 'admin'  # Tvoje jméno
-email = 'admin@admin.cz'
-password = 'TvojeHeslo123' # Tvoje heslo
+username = 'admin'
+email = 'elektrobrn22@gmail.com'
+password = 'djangO8109'
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
