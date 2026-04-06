3266de5db603473a8434efc3a39ad6b1d2b1f906
JustDav3 <elektrobrn22@gmail.com>
2026-04-06 15:11:03 +0000

    databaze

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,13 +75,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-db_path = os.environ.get('data', os.path.join(BASE_DIR, 'data_np00'))
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': db_path,
-    }
+    'default': dj_database_url.config(
+        # Pokud DATABASE_URL na Renderu existuje, použije ji.
+        # Pokud ne (třeba u tebe v PC), použije se lokální sqlite.
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
