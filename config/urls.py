5f47c007796c950446adfba375b70ef740c03e60
JustDav3 <elektrobrn22@gmail.com>
2026-04-12 23:03:23 +0000

    beta verze 0.5 - Fix

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path
 from main.views import *
 from django.contrib.auth.views import LogoutView
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
